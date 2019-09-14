889d157e3de86cae406fdd9799fb1db902cc3a66
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2019-09-13 22:06:33 -0400

    Refactoring

--- src/flickr-fetcher.js
+++ src/flickr-fetcher.js
@@ -1,7 +1,11 @@
 var FlickrFetcher = {
   photoObjToURL: function (photoObj) {
-    return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' +
-            photoObj.secret + '_b.jpg'
+    return [ 'https://farm',
+      photoObj.farm, '.staticflickr.com/',
+      photoObj.server, '/',
+      photoObj.id, '_',
+      photoObj.secret, '_b.jpg'
+    ].join('')
   }
 }
 
