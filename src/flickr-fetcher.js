1f128f88024a973011dfd8da0f714f7b052d7588
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2019-09-13 21:59:36 -0400

    Add another test and fix code.

--- src/flickr-fetcher.js
+++ src/flickr-fetcher.js
@@ -1,6 +1,7 @@
 var FlickrFetcher = {
-  photoObjToURL: function () {
-    return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg'
+  photoObjToURL: function (photoObj) {
+    return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' +
+            photoObj.secret + '_b.jpg'
   }
 }
 
--- test/flickr-fetcher-spec.js
+++ test/flickr-fetcher-spec.js
@@ -3,8 +3,8 @@ var expect = require('chai').expect
 
 var FlickrFetcher = require('../src/flickr-fetcher.js')
 
-describe('#photoObjToURL()', function () {
-  it('should take a photo object from Flickr and return a string', function () {
+describe('#photoObjToURL() first', function () {
+  it('should take photo 24770505034 object from Flickr and return a string', function () {
     var input = {
       id: '24770505034',
       owner: '97248275@N03',
@@ -16,7 +16,27 @@ describe('#photoObjToURL()', function () {
       isfriend: 0,
       isfamily: 0
     }
+
     var expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg'
+    var actual = FlickrFetcher.photoObjToURL(input)
+    expect(actual).to.eql(expected)
+  })
+})
+
+describe('#photoObjToURL() second', function () {
+  it('should take photo 24770504484 object from Flickr and return a string', function () {
+    var input = {
+      id: '24770504484',
+      owner: '97248275@N03',
+      secret: '69dd90d5dd',
+      server: '1451',
+      farm: 2,
+      title: '20160229090903',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    }
+    var expected = 'https://farm2.staticflickr.com/1451/24770504484_69dd90d5dd_b.jpg'
     var actual = FlickrFetcher.photoObjToURL(input)
     expect(actual).to.eql(expected)
   })
